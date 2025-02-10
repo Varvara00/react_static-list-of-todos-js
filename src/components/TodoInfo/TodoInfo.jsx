ea899c874b5c1bafc26847bd76d020d19ea60dcb
Varvara00 <varvara.galy171203@gmail.com>
2025-02-10 16:22:10 +0800

    finished task react_static-list-of-todos

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const TodoInfo = ({ todo }) => (
-  <article className="TodoInfo TodoInfo--completed">
+  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && todo.user.email && (
       <a className="UserInfo" href={`mailto:${todo.user.email}`}>
